300cbbd4213bc431515482ab8fc5227fab2a1728
Abhishek Mane <abhishekmane@outlook.in>
2021-01-14 19:34:25 +0530

    mongo service

---
+++ src/services/mongodb.ts
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+
+export class MongoDBService {
+  private static instance: MongoDBService;
+  private readonly mongo = new MongoClient(process.env["MONGO_URI"] || "", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  private async init() {
+    await this.mongo.connect();
+  }
+
+  public getCollection(dbName, collectionName) {
+    const db = this.mongo.db(dbName);
+    return db.collection(collectionName);
+  }
+
+  public static async getInstance(): Promise<MongoDBService> {
+    if (!MongoDBService.instance) {
+      MongoDBService.instance = new MongoDBService();
+      await MongoDBService.instance.init();
+    }
+    return MongoDBService.instance;
+  }
+}
