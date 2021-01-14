fb978d79cf7184a5b1f392a69be084ca54cb4b3a
Abhishek Mane <abhishekmane@outlook.in>
2021-01-14 19:35:43 +0530

    gql server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hey, this is it");
+import { ApolloServer } from "apollo-server";
+import { resolvers } from "./resolvers";
+import { typeDefs } from "./typedefs";
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => console.log(`GraphQL Service running on ${url}`));
